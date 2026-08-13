1f8c00fbe2b6dda236dedb91644d50dd639e56c4
Alison <alicamelo23@gmail.com>
2026-08-13 17:57:29 +0200

    adding the function check_flags

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -3,7 +3,6 @@
 
 # include <limits.h>
 # include <stdlib.h>
-# include <unistd.h>
 # include <stdarg.h>
 # include <stdio.h>
 # include <unistd.h>
@@ -11,8 +10,19 @@
 
 typedef struct s_stack 
 {
-    int integers;
+    int value;
+    struct s_tack *prev;
+    struct s_stack *next;
 } t_stack;
+
+typedef struct s_flags
+{
+    int simple;
+    int medium;
+    int complex;
+    int adaptative;
+    int bench;
+} t_flags;
 
 int     validate_argv(char **argv);
 
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: acamelo <acamelo@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/11 17:42:08 by acamelo           #+#    #+#             */
-/*   Updated: 2026/08/12 18:22:49 by acamelo          ###   ########.fr       */
+/*   Updated: 2026/08/13 17:53:12 by acamelo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,26 @@ int push_swap(char *argv)
     }
     return(1);    
 }
+int check_flags(char *arg, t_flags flags)
+{
+    if (ft_strcmp(arg, "--simple") == 0)
+        return (flags->simple = 1, 1);
+    if (ft_strcmp(arg, "--medium") == 0)
+        return (flags->medium = 1, 1);
+    if (ft_strcmp(arg, "--complex") == 0)
+        return (flags->complex = 1, 1);
+    if (ft_strcmp(arg, "--adaptive") == 0)
+        return (flags->adaptive = 1, 1);
+    if (ft_strcmp(arg, "--bench") == 0)
+        return (flags->bench = 1, 1);
+    return(0);
+}
 int main(int argc, char **argv)
 {
     int var;
 
     if (argc < 2)
-    {
-        ft_printf("ERROR");
-        return(1);
-    }
+        return(0);
     var = validate_argv(argv);
     if (var == 1)
         ft_printf("Error");
