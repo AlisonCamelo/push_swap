f49d388ce76ad613cca65a831c9b57adad16702f
Alison Camelo <acamelo@student.42madrid.com>
2026-08-14 12:01:19 +0200

    adding flags to the code

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: acamelo <acamelo@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/11 17:42:08 by acamelo           #+#    #+#             */
-/*   Updated: 2026/08/13 19:05:15 by acamelo          ###   ########.fr       */
+/*   Updated: 2026/08/14 12:00:18 by acamelo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void init_flags(t_flags *flags)
 }
 
 // funcion que se encarga unicamente de detectar flags
-int check_flags(char *arg, t_flags flags)
+int check_flags(char *arg, t_flags *flags)
 {
     if (ft_strcmp(arg, "--simple") == 0)
         return (flags->simple = 1, 1);
@@ -32,7 +32,7 @@ int check_flags(char *arg, t_flags flags)
     if (ft_strcmp(arg, "--complex") == 0)
         return (flags->complex = 1, 1);
     if (ft_strcmp(arg, "--adaptive") == 0)
-        return (flags->adaptive = 1, 1);
+        return (flags->adaptative = 1, 1);
     if (ft_strcmp(arg, "--bench") == 0)
         return (flags->bench = 1, 1);
     return(0);
@@ -46,7 +46,7 @@ int main(int argc, char **argv)
 {
     t_stack *stack_a;
     t_stack *stack_b;
-    int stack_size;
+    t_flags flags;
     int i;
 
     i = 1;
@@ -54,6 +54,15 @@ int main(int argc, char **argv)
     stack_b = NULL;
     if (argc < 2)
         return(0);
+    init_flags(&flags);
+    while(argv[i] != NULL)
+    {
+        if (check_flags(argv[i], &flags))
+        {
+            i++;
+            
+        }
+    }
     var = validate_argv(argv);
     if (var == 1)
         ft_printf("Error");
