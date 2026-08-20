8136ae93ef71603d449b2ae2d9ba5493b167a84c
Alison Camelo <acamelo@student.42madrid.com>
2026-08-20 19:34:37 +0200

    creting stack utils functions

--- src/parsing/parse_nums.c
+++ src/parsing/parse_nums.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils_pushswap.c                                   :+:      :+:    :+:   */
+/*   parse_nums.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acamelo <acamelo@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/18 16:44:26 by acamelo           #+#    #+#             */
-/*   Updated: 2026/08/20 17:55:30 by acamelo          ###   ########.fr       */
+/*   Updated: 2026/08/20 18:01:49 by acamelo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,33 +56,3 @@ int check_duplicates(t_stack *stack, int num)
     }
     return(0);//no hay duplicados
 }
-//funcion que crea y retorna un nuevo nodo
-/*t_stack *stack_new(int num)
-{
-    t_stack *new_node;
-    
-    new_node = malloc(sizeof(t_stack));
-    if (!new_node)
-        return(NULL);
-    new_node->value = num;//
-    new_node->prev = NULL;
-    new_node->next = NULL;
-    return(new_node);
-}
-void stack_add_back(t_stack **stack, t_stack *new_node)
-{
-    t_stack *last;
-    
-    if (!stack || !new_node)
-        return ;
-    if (*stack ==  NULL)
-    {
-        *stack = new_node;
-        return ;
-    }
-    last = *stack;
-    while(last->next != NULL)
-        last = last->next;
-    last->next = new_node;
-    new_node->prev = last;
-}*/
---
+++ src/parsing/utils_stack.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_stack.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acamelo <acamelo@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/19 21:14:10 by acamelo           #+#    #+#             */
+/*   Updated: 2026/08/20 19:32:48 by acamelo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "inc/push_swap.h"
+
+//funcion que crea y retorna un nuevo nodo
+t_stack *stack_new(int num)
+{
+    t_stack *node;
+    
+    node = (t_stack *)malloc(sizeof(t_stack));
+    if (!node)
+        return(NULL);
+    node->value = num;
+    node->index = -1;
+    node->next = NULL;
+    return(node);
+}
+void stack_add_back(t_stack **stack, t_stack *new_node)
+{
+    t_stack *last;
+    
+    if (!stack || !new_node)
+        return ;
+    if (*stack ==  NULL)
+    {
+        *stack = new_node;
+        return ;
+    }
+    last = *stack;
+    while(last->next != NULL)
+        last = last->next;
+    last->next = new_node;
+}
+void free_stack(t_stack **stack)
+{
+    t_stack *tmp;
+    
+    if(!stack || !*stack)
+        return;
+    while(*stack)
+    {
+        tmp = (*stack)->next;
+        free(*stack);
+        *stack = tmp;
+    } 
+    *stack = NULL;
+}
+void free_split(char **split)
+{
+    int i;
+    
+    if(!split)
+        return ;
+    i = 0;
+    while(split[i])
+    {
+        free(split[i]);
+        i++;
+    }
+    free(split);
+}
