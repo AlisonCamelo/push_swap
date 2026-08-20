661d77f847f65ce656dca4519b5dc1c06df8abd5
Alison Camelo <acamelo@student.42madrid.com>
2026-08-20 17:36:35 +0200

    updating parse_str_args

--- push_swap_valid_args.c
+++ push_swap_valid_args.c
@@ -6,7 +6,7 @@
 /*   By: acamelo <acamelo@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/19 11:23:44 by acamelo           #+#    #+#             */
-/*   Updated: 2026/08/20 17:31:36 by acamelo          ###   ########.fr       */
+/*   Updated: 2026/08/20 17:36:25 by acamelo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,19 +29,14 @@ static int process_flags(char *arg, t_flags *flags)
         return(0);
     return(1);
 }
-//funcion que determina si los argumentos son numeros validos a demas una ves hace el split y los demas chequeos
-//hace un join que lo que permite es unir argumento por argumento al final de la lista list_str seguido de un espacio
-static int check_valid_num(char *argv, char **list_str, t_flags *flags)
+static int parse_str_argv(char *arg, t_stack **stack_a)
 {
     char **nums;
     int i;
-    while(*argv == ' ') //si hay espacios, continua, esto por si alguien solo manda espacios como argumentos
-        argv++;
-    if(!*argv) //si argv se quedo vacio return(0)
-        return(0);
-    nums = ft_split(argv, ' ');
-    if(!nums) // split fallo? retorna
-        return(0);
+ 
+    nums = ft_split(arg, ' ');
+    if(!nums || !*nums) // split fallo? retorna
+        return(free_split(nums), 0);
     i = 0;
     while(nums[i])
     {
@@ -49,23 +44,8 @@ static int check_valid_num(char *argv, char **list_str, t_flags *flags)
             return(ft_free(nums), 0); //si falla libera memoria y retorna(0)
     }
     ft_free(nums); //libera todo el split(ya valido todos los textos)
-    if(flags->parser_nums == 1) // si hubo algun error de logica/etc..
-        return(0); //hubo error retorna 0
-    flags->parser_nums = 0; //si por el contraro no hubo error al leer nada parser sera 0
-    return(1); //se ha completado con exito
 }
-//funcion que muestra error y que libera memoria si aun no ha sido liberada correctamente
-static void *ft_free_and_error(char **list_str)
-{
-    write(2, "Error\n", 6);
-    if(list_str && *list_str)
-    {
-        free(*list_str);
-        *list_str = NULL;
-    }
-    return(NULL);
-}
- //
+
 int parse_args(int argc, char **argv, t_stack **stack_a, t_flags *flags)
 {
     int i;
