1be657331a4a4b20d2990def050e25eb474892a3
Alison Camelo <acamelo@student.42madrid.com>
2026-08-20 21:22:21 +0200

    adding disorder metrics

--- src/metrics/disorder.c
+++ src/metrics/disorder.c
@@ -6,16 +6,48 @@
 /*   By: acamelo <acamelo@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/20 20:47:39 by acamelo           #+#    #+#             */
-/*   Updated: 2026/08/20 21:11:22 by acamelo          ###   ########.fr       */
+/*   Updated: 2026/08/20 21:22:01 by acamelo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/push_swap.h"
 
+static int count_inversions(t_stack *stack)
+{
+    t_stack *i;
+    t_stack *j;
+    int inversions;
+
+    inversions = 0;
+    i = stack;
+    while(i)
+    {
+        j = i->next;
+        while(j)
+        {
+            if(i->value > j->value)
+                inversions++;
+            j = j->next;
+        }
+        i = i->next;
+    }
+    return(inversions);
+}
 // Devuelve el porcentaje de desorden de 0 a 100 (int)
 int	calculate_disorder(t_stack *stack)
 {
-    
+    int size;
+    int inversions;
+    int max_inversions;
+
+    size = ft_stack_size(stack);
+    if(size < 1)
+        return(0);
+    inversions = count_inversions(stack);
+    max_inversions = (size * (size - 1)) / 2;
+    if(max_inversions == 0)
+        return(0);
+    return((inversions * 100) / max_inversions);
 }
 // Comprueba si la lista ya está completamente ordenada
 int is_sorted(t_stack *stack)
