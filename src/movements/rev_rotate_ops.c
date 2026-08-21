5f120b85a100b7ddde72c0b45e506407fccdf4c9
Alison Camelo <acamelo@student.42madrid.com>
2026-08-21 13:45:26 +0200

    adding revers rotate a function

--- src/movements/rev_rotate_ops.c
+++ src/movements/rev_rotate_ops.c
@@ -6,7 +6,7 @@
 /*   By: acamelo <acamelo@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/21 10:34:09 by acamelo           #+#    #+#             */
-/*   Updated: 2026/08/21 13:25:54 by acamelo          ###   ########.fr       */
+/*   Updated: 2026/08/21 13:44:50 by acamelo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,23 @@
 
 int	rra(t_stack **stack_a)
 {
-    
+    t_stack *last;
+    t_stack *first;
+
+    while(!stack_a || !*stack_a || !(*stack_a)->next)
+        return(0);
+    last = NULL;
+    first = *stack_a;
+    while(first->next)
+    {
+        last = first;
+        first = first->next;
+    }
+    last->next = NULL;
+    first->next = *stack_a;
+    *stack_a = first;
+    write(1, "rra\n", 4);
+    return(1);
 }
 int	rrb(t_stack **stack_b)
 {
